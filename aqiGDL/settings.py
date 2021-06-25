c58e8f7a313e5faef7f7d67de3fab28411ed756d
Luis Guillermo Natera Orozco <nateraluis@gmail.com>
2021-06-25 18:01:10 +0200

    Changes to DB details and Load graph to DB function

--- aqiGDL/settings.py
+++ aqiGDL/settings.py
@@ -12,14 +12,8 @@ log_level = lg.INFO
 log_name = "aireGDL"
 log_filename = "aireGDL"
 
-# Database settings old
-url = 'airegdlpip.cptlhu1n34ei.us-west-1.rds.amazonaws.com'
-user = 'postgres'
-pw = 'pipguadalajara'
-db = 'postgres'
-
 # Database settings
-# url = 'pip-gdl.cxkuumj8eis0.us-west-2.rds.amazonaws.com'
-# user = 'pipderive'
-# pw = 'aireGuadalajara'
-# db = 'postgres'
+url = 'pip-gdl.cxkuumj8eis0.us-west-2.rds.amazonaws.com'
+user = 'pipderive'
+pw = 'aireGuadalajara'
+db = 'postgres'
